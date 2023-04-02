5c312daa33bdb372ca37a2e96f7bfdd139f5a718
klc-pakize <pa3kize@gmail.com>
2023-04-02 18:08:38 +0300

    fexs: updated function types

--- 012-functions.py
+++ 012-functions.py
@@ -193,3 +193,201 @@ def change_3():
 
 change_3()  # 45
 print(k)  # 45
+
+
+#! Fonksiyon Türleri | Function Types:
+#? Built-in Functions:(Dahili Fonksiyonlar): 
+# Python'da zaten tanımlanmış ve hazır olarak kullanılabilecek birçok dahili işlev vardır. 
+# Bu fonksiyonların en sık kullanılanları arasında "print()", "len()", "input()", "range()", "open()" ve "str()" gibi fonksiyonlar yer alır.
+
+# Python has many built-in functions already defined and readily available.
+# Among the most common of these functions are functions such as "print()", "len()", "input()", "range()", "open()" and "str()".
+
+#? print(): 
+# Verilen argümanları standart çıktıya yazdırmak için kullanılır. | Used to print the given arguments to standard output.
+print("Merhaba Dünya!") # Merhaba Dünya! 
+
+
+#? len(): 
+# Bir nesnenin uzunluğunu hesaplamak için kullanılır. | It is used to calculate the length of an object.
+liste = [1, 2, 3, 4, 5]
+print(len(liste)) # 5 
+
+
+#? type(): 
+# Bir nesnenin türünü döndürmek için kullanılır. | Used to return the type of an object.
+sayi = 10
+print(type(sayi)) # <class 'int'> 
+
+
+#? range(): 
+# Bir aralık nesnesi oluşturmak için kullanılır. | It is used to create a range object.
+for i in range(1, 6):
+    print(i) # 1, 2, 3, 4, 5 
+
+
+#? zip(): 
+# Verilen iterable nesnelerden eşleştirilmiş bir nesne oluşturmak için kullanılır.
+# It is used to create a matched object from the given iterable objects.
+liste1 = [1, 2, 3]
+liste2 = ['a', 'b', 'c']
+liste3 = [True, False, True]
+
+for x in zip(liste1, liste2, liste3):
+    print(x) # (1, 'a', True), (2, 'b', False), (3, 'c', True)
+
+
+#? sum(): 
+# Verilen sayısal değerlerin toplamını hesaplamak için kullanılır. | It is used to calculate the sum of the given numeric values.
+liste = [1, 2, 3, 4, 5]
+print(sum(liste)) # 15
+
+
+#? max(): 
+# Verilen sayısal değerlerin en büyük değerini döndürmek için kullanılır. | It is used to return the largest value of the given numeric values.
+liste = [1, 2, 3, 4, 5]
+print(max(liste)) # 5 
+
+
+#? min(): 
+# Verilen sayısal değerlerin en küçük değerini döndürmek için kullanılır. | It is used to return the smallest value of the given numeric values.
+liste = [1, 2, 3, 4, 5]
+print(min(liste)) # 1
+
+
+#? all(): 
+# Bu fonksiyon, bir iterable'ın tüm elemanlarının "True" olduğunu kontrol eder. | This function checks that all elements of an iterable are "True".
+# Eğer tüm elemanlar "True" ise, fonksiyon "True" değerini döndürür. | If all elements are "True", the function returns "True".
+# Aksi takdirde, "False" değerini döndürür. | Otherwise, it returns "False".
+numbers = [2, 4, 6, 8, 10]
+result = all(num % 2 == 0 for num in numbers)
+print(result)  # True
+
+numbers2 = [2, 3, 4, 6, 8, 10]
+result2 = all(num % 2 == 0 for num in numbers2)
+print(result2)  # False
+
+
+#? any(): 
+# Bu fonksiyon, bir iterable'ın en az bir elemanının "True" olduğunu kontrol eder. | This function checks that at least one element of an iterable is "True".
+# Eğer en az bir eleman "True" ise, fonksiyon "True" değerini döndürür. | If at least one element is "True", the function returns "True".
+# Eğer iterable'da hiçbir eleman "True" değilse, "False" değerini döndürür. | Returns "False" if no element in the iterable is "True".
+numbers = [1, 3, 5, 7, 8]
+result = any(num % 2 == 0 for num in numbers)
+print(result)  # True
+
+numbers2 = [1, 3, 5, 7, 9]
+result2 = any(num % 2 == 0 for num in numbers2)
+print(result2)  # False
+
+
+#? filter(): 
+# Bu fonksiyon, verilen bir fonksiyonu bir iterable nesnenin her elemanına uygular 
+# ve sadece fonksiyonun "True" olarak değerlendirdiği elemanları içeren bir iterator nesnesi döndürür.
+
+# This function applies a given function to every element of an iterable object
+# and returns an iterator object containing only elements that the function considers "True".
+numbers = [1, 2, 3, 4, 5, 6]
+even_numbers = filter(lambda x: x % 2 == 0, numbers)
+print(list(even_numbers))  # [2, 4, 6]
+
+words = ["apple", "banana", "cherry", "date"]
+short_words = filter(lambda x: len(x) < 6, words)
+print(list(short_words))  # ['apple', 'cherry', 'date']
+
+
+#? round(): 
+# Bu fonksiyon, verilen sayıyı belirtilen bir ondalık basamağa yuvarlar. | This function rounds the given number to a specified decimal place.
+# Varsayılan olarak, fonksiyon sayıyı en yakın tamsayıya yuvarlar. | By default, the function rounds the number to the nearest integer.
+# Ondalık basamak sayısını belirtmek için, fonksiyona bir ikinci argüman olarak bir tam sayı verilebilir. | An integer can be given as a second argument to the function to specify the number of decimal places.
+number = 3.14159265359
+rounded = round(number, 2)
+print(rounded)  # 3.14
+
+number2 = 3.5
+rounded2 = round(number2)
+print(rounded2)  # 4
+
+
+#? int(): 
+# Bir sayısal değerin tamsayı kısmını döndürmek için kullanılır. | Used to return the integer part of a numeric value.
+ondalikli_sayi = 5.9
+print(int(ondalikli_sayi)) # 5 
+
+
+#? float(): 
+# Bir sayısal değerin ondalık kısmını döndürmek için kullanılır. | Used to return the decimal part of a numeric value.
+tamsayi = 5
+print(float(tamsayi)) # 5.0
+
+
+#? str(): 
+# Bir nesnenin karakter dizisi temsilini döndürmek için kullanılır. | Used to return the string representation of an object.
+sayi = 10
+print(str(sayi) + " tane elma var.") # 10 tane elma var.
+
+
+#? list(): 
+# Bir liste nesnesi oluşturmak için kullanılır. | It is used to create a list object.
+liste = [1, 2, 3, 4, 5]
+print(liste) # [1, 2, 3, 4, 5]
+
+
+#? dict(): 
+# Bir sözlük nesnesi oluşturmak için kullanılır. | It is used to create a dictionary object.
+sozluk = {"ad": "Ahmet", "soyad": "Yılmaz", "yas": 25}
+print(sozluk) # {'ad': 'Ahmet', 'soyad': 'Yılmaz', 'yas': 25} 
+
+
+#? set(): 
+# Bir küme nesnesi oluşturmak için kullanılır. | It is used to create a set object.
+kume = {1, 2, 3, 4, 5}
+print(kume) # {1, 2, 3, 4, 5}
+
+
+#? tuple(): 
+# Bir tuple nesnesi oluşturmak için kullanılır. | # Used to create a tuple object.
+tuple = (1, 2, 3, 4, 5)
+print(tuple) # (1, 2, 3, 4, 5) 
+
+
+#* Şuana kadar yazıklarımız!!!!
+#? User-defined Functions (Kullanıcı Tanımlı Fonksiyonlar): 
+# Python'da, kullanıcılar kendi işlevlerini tanımlayabilir ve kullanabilir. 
+# Bu tür işlevler, programın daha modüler ve okunaklı olmasına yardımcı olur.
+
+# In Python, users can define and use their own functions.
+# Such functions help the program to be more modular and readable.
+
+
+#? Anonymous Functions (Lambda Fonksiyonları): 
+# Python'da, lambda ifadesi kullanılarak anonim işlevler oluşturulabilir. 
+# Bu tür işlevler genellikle tek bir satırlık işlemler için kullanılır ve lambda ifadesi kullanılarak tanımlanırlar.
+
+# In Python, anonymous functions can be created using the lambda expression.
+# Such functions are usually used for single-line operations and are defined using a lambda expression.
+
+#* İki sayıyı toplamak için lambda fonksiyonu kullanma:
+addition = lambda x, y: x + y
+print(addition(3, 5)) # Output: 8
+
+#* Bir listenin elemanlarını iki katına çıkarmak için lambda fonksiyonu kullanma:
+my_list = [1, 2, 3, 4, 5]
+doubled_list = list(map(lambda x: x * 2, my_list))
+print(doubled_list) # Output: [2, 4, 6, 8, 10]
+
+#* Bir listedeki sayıları filtrelemek için lambda fonksiyonu kullanma:
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+filtered_list = list(filter(lambda x: x % 2 == 0, my_list))
+print(filtered_list) # Output: [2, 4, 6, 8, 10]
+
+#* İki listenin elemanlarını toplayarak yeni bir liste oluşturmak için lambda fonksiyonu kullanma:
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+result = list(map(lambda x, y: x + y, list1, list2))
+print(result) # Output: [5, 7, 9]
+
+#* Bir dict. değerleri sıralamak için lambda fonksiyonu kullanma:
+my_dict = {'a': 3, 'b': 1, 'c': 2}
+sorted_dict = dict(sorted(my_dict.items(), key=lambda x: x[1]))
+print(sorted_dict) # Output: {'b': 1, 'c': 2, 'a': 
