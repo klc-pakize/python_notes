523178c12e38f400b0b4d099f6bdf7e2858d22f7
klc-pakize <pa3kize@gmail.com>
2023-04-03 17:20:46 +0300

    fexs: updated data types

--- 003-data_types.py
+++ 003-data_types.py
@@ -40,6 +40,58 @@ sum = x + y
 print(sum)  # 30
 print(type(sum))  # <class 'int'>
 
+#! Python Numaraları
+#? Python'da üç sayısal tür vardır: | There are three numeric types in Python:
+# 1- int
+# 2- float
+# 3- complex
+# Sayısal tipteki değişkenler, onlara bir değer atadığınızda oluşturulur: | Variables of numeric types are created when you assign a value to them:
+x = 1
+y = 2.8
+z = 1j 
+print(type(x))  # int
+print(type(y))  # float
+print(type(z))  # complex
+
+#? Int 
+# Int veya tamsayı, pozitif veya negatif, ondalık basamak içermeyen, sınırsız uzunlukta bir tam sayıdır. | Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))  # int
+print(type(y))  # int
+print(type(z))  # int
+
+#? Float
+# Bir veya daha fazla ondalık basamak içeren pozitif veya negatif bir sayıdır. | Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+# Float, 10'un kuvvetini belirtmek için "e" harfi bulunan bilimsel sayılar da olabilir. | Float can also be scientific numbers with an "e" to indicate the power of 10.
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))  # float
+print(type(y))  # float
+print(type(z))  # float
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#? Complex
+# Karmaşık sayılar sanal kısım olarak "j" ile yazılır: | Complex numbers are written with a "j" as the imaginary part:
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))  # complex
+print(type(y))  # complex
+print(type(z))  # complex
 
 #! Tip Dönüşümü:
 #! Type Conversion:
@@ -53,6 +105,10 @@ print(type(age))  # <class 'int'>
 print(type(weight))  # <class 'float'>
 print(type(name))  # <class 'str'>
 print(type(isStudent))  # <class 'bool'>
+
+#! Not: Karmaşık sayıları başka bir sayı türüne dönüştüremezsiniz.
+#! Note: You cannot convert complex numbers into another number type.
+
 
 #? int to float
 resault = float(age)  
@@ -88,6 +144,15 @@ print(type(resault6))  # <class 'str'>
 resault7 = str(weight)
 print(resault7)  # 48.77
 print(type(resault7))  # <class 'str'>
+
+#? Rastgele Sayı
+# Python'un rasgele bir sayı yapmak için bir random() işlevi yoktur, | Python does not have a random() function to make a random number, 
+# ancak Python'un rasgele sayılar yapmak için kullanılabilecek random adlı yerleşik bir modülü vardır: | but Python has a built-in module called random that can be used to make random numbers:
+
+# 1 ile 9 arasında rastgele bir sayı görüntüleyin: | display a random number between 1 and 9:
+import random
+
+print(random.randrange(1, 10))
 
 
 #! EXAMPLES:
